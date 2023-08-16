9754213901ff561302de9ad44eda8c737f05fa2d
Tommy Henderson <tommyhenderson@tommys-mbp.lan>
2023-08-16 15:36:44 -0500

    added more javascript to display the first question/answers

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,54 @@
-// Click the start button
-// timer starts and presented with a question
-//if answer the question correctly, then moves on to the next question
-// if answer incorrectly, it subtracts time from timer
-// always says either wrong or right depending on answer
-
 var questions = [
     {
         question: "Inside which HTML Element do we put the JavaScript?",
-        answers: [
-            {text: "<script>", correct: true},
-            {text: "<js>", correct: false},
-            {text: "<javascript>", correct: false},
-            {text: "<scripting>", correct: false},
-        ]
+        a: "<script>",
+        b: "<js>",
+        c: "<javascript>",
+        d: "<scripting>",
+        correct: "a",
     },
     {
         question: "How do you create a function in JavaScript?",
-        answers: [
-            {text: "function:myFunction()", correct: false},
-            {text: "function = myFunction()", correct: false},
-            {text: "function myFunction()", correct: true},
-            {text: "myFunction()", correct: false},
-        ] 
+        a: "function:myFunction()",
+        b: "function = myFunction()",
+        c: "function myFunction()",
+        d: "myFunction()",
+        correct: "c",
     },
     {
-        question: "Where is the correct place to insert a JavaScript?",
-        answers: [
-            {text: "the <body> section", correct: false},
-            {text: "the <head> section", correct: false},
-            {text: "Both the <head> section and the <body> section are correct", correct: true},
-            {text: "Neither the <head> nor the <body> sections are correct", correct: false},
-        ] 
+        question: "Where is the correct place to insert JavaScript",
+        a: "the <body> section",
+        b: "the <head> section",
+        c: "Both the <head> section and the <body> section are correct",
+        d: "Neither the <head> nor the <body> sections are correct",
+        correct: "c",
     },
-]; 
+];
+
+var questionEl = document.getElementById("question")
+var answer1 = document.getElementById("answer1")
+var answer2 = document.getElementById("answer2")
+var answer3 = document.getElementById("answer3")
+var answer4 = document.getElementById("answer4")
+
+var nextButton = document.getElementById("nextbutton")
+
+var currentQuestion = 0;
+var score = 0;
+
+function startQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    loadQuestion()
+}
+
+function loadQuestion() {
+    questionEl.textContent = questions[currentQuestion].question
+    answer1.textContent = questions[currentQuestion].a
+    answer2.textContent = questions[currentQuestion].b
+    answer3.textContent = questions[currentQuestion].c
+    answer4.textContent = questions[currentQuestion].d
+}
+
+startQuiz()
